d41237ca4f62c949c100a059339eead2ba25b0d7
jfails1 <jfails@byu.edu>
2024-03-22 14:18:40 -0600

    Add project files.

---
+++ Mission11_Fails/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Fails.Models;
+using Mission11_Fails.Models.ViewModels;
+using System.Diagnostics;
+
+namespace Mission11_Fails.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookRepository _repo;
+        public HomeController(IBookRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index(int pageNum)
+        {
+            int pageSize = 10;
+
+            var blah = new BookListViewModel
+            {
+                Books = _repo.Books
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+                }
+            };
+
+            return View(blah);
+        }
+    }
+}
---
+++ Mission11_Fails/Models/IBookRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Mission11_Fails.Models
+{
+    public interface IBookRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
